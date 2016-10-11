46e521b6c283b7ec74513ebbf7eb0c0a0f975394
Will Brennan <galacticonrex@yahoo.com>
2016-10-11 18:02:52 -0400

    Fixed some animation problems

---
+++ EGDFinalProject/Assets/scripts/AnimationTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Animator anim = GetComponent<Animator>();
+        anim.SetBool("IsMoving", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
